287065f75f056da1d31b771fe3af7f886f4a56ae
hanneskiene <hannes-kiene@live.de>
2019-10-07 12:13:47 +0200

    removed empty constructor; added const to filetree member func

--- src/commands.hpp
+++ src/commands.hpp
@@ -6,26 +6,19 @@ namespace ssf
 // vim - VimCommand
 // test.txt - file
 // Both should autocomplete or list options when the user hits TAB
-// Command Objects can (or should) be daisy-chained together: file is child object of vimCommand  
+// Command Objects can (or should) be daisy-chained together: file is child object of vimCommand
 class CommandObject
 {
 public:
-	
 	//Each CommandObject implements:
-	//set_text THIS IS THE COMMAND TEXT (COMPLETE OR NOT)	
+	//set_text THIS IS THE COMMAND TEXT (COMPLETE OR NOT)
 	//get_autocompletion()
 	//is_valid()
 	//callback_t(vector)
-	//evaluate()	
+	//evaluate()
 
 private:
-	//List of child CommandObjects	
+	//List of child CommandObjects
+};
 
-
-
-
-}
-
-
-
-}
+} // namespace ssf
--- src/filetree.hpp
+++ src/filetree.hpp
@@ -10,161 +10,171 @@
 namespace ssf
 {
 
-	//
-	// This class is supposed to handle all the filesystem interactions using std::filesystem
-	//
-	class Filetree
+//
+// This class is supposed to handle all the filesystem interactions using std::filesystem
+//
+class Filetree
+{
+public:
+	Filetree()
 	{
-		public:
-			Filetree()
-			{
-				log << "init path: " << std::filesystem::current_path() << "\n";
-				current_path = std::filesystem::absolute(std::filesystem::current_path());
-			}
-			~Filetree()
-			{
-			}
+		log << "init path: " << std::filesystem::current_path() << "\n";
+		current_path = std::filesystem::absolute(std::filesystem::current_path());
+	}
 
-			auto move_layer_up()
-			{
-				if (current_path.has_parent_path())
-				{
-					log << "current Path :" << current_path << "\n";
-					current_path = current_path.parent_path();
-					log << "parent Path : " << current_path << "\n";
-				}
-			}
+	auto move_layer_up()
+	{
+		if (current_path.has_parent_path())
+		{
+			log << "current Path :" << current_path << "\n";
+			current_path = current_path.parent_path();
+			log << "parent Path : " << current_path << "\n";
+		}
+	}
 
-			void move_up()
+	void move_up()
+	{
+		//  Sanitize
+		if (selection != 0)
+		{
+			selection--;
+		}
+	}
+	void move_down()
+	{
+		auto di = std::filesystem::directory_iterator(current_path);
+		// Sanitize
+		int dist = std::distance(std::filesystem::begin(di), std::filesystem::end(di));
+		if (selection < dist - 1)
+		{
+			selection++;
+		}
+	}
+	void move_left()
+	{
+
+		if (current_path.has_relative_path())
+		{
+			current_path = current_path.parent_path();
+			selection = 0;
+		}
+		else
+		{
+			//has no relative path -> in the root dir
+		}
+	}
+	void move_right()
+	{
+		try
+		{
+			auto selected = get_directory_entry(selection);
+			if (selected.is_directory())
 			{
-				//  Sanitize
-				if(selection != 0){
-					selection--;
-				}
+				auto temp_path = current_path / selected.path().filename();
+				auto di = std::filesystem::directory_iterator(temp_path);
+				selection = 0;
+				current_path = temp_path;
 			}
-			void move_down()
+		}
+		catch (std::filesystem::filesystem_error &e)
+		{
+		}
+	}
+
+	auto get_current() const
+	{
+		try
+		{
+			return get_directory_list(current_path);
+		}
+		catch (std::filesystem::filesystem_error &e)
+		{
+			log << "Exception occured!!: " << e.what() << "\n";
+			return std::vector<std::string>();
+		}
+	}
+	auto get_selection() const
+	{
+		return selection;
+	}
+
+	auto get_left() const
+	{
+		auto temp = current_path;
+		if (temp.has_relative_path())
+		{
+			temp = temp.parent_path();
+			auto di = std::filesystem::directory_iterator(temp);
+			std::vector<std::string> res;
+			for (auto &d : di)
 			{
-				auto di = std::filesystem::directory_iterator(current_path);
-				// Sanitize
-				int dist = std::distance(std::filesystem::begin(di),std::filesystem::end(di));
-				if(selection < dist-1){
-					selection++;
-				}
+				res.push_back(d.path().filename().string());
 			}
-			void move_left()
+			return res;
+		}
+		return std::vector<std::string>();
+	}
+
+	// TODO: Remove cascaded try/catch and multiple default returns
+	auto get_right() const
+	{
+		try
+		{
+			auto selected = get_directory_entry(selection);
+			log << "GET RIGHT: " << selected.path().filename().string() << "\n";
+
+			if (selected.is_directory())
 			{
 
-
-				if (current_path.has_relative_path())
-				{
-					current_path = current_path.parent_path();
-					selection = 0;
-				}else{
-					//has no relative path -> in the root dir	
-				}
-			}
-			void move_right()
-			{
-				try{
-					auto selected = get_directory_entry(selection);
-					if (selected.is_directory())
-					{
-						auto temp_path = current_path / selected.path().filename();
-						auto di = std::filesystem::directory_iterator(temp_path);
-						selection = 0;
-						current_path = temp_path;
-					}
-				}catch (std::filesystem::filesystem_error& e){
-
-				}
-			}
-
-			auto get_current()
-			{
+				auto temp_path = current_path;
+				temp_path = temp_path / selected.path().filename();
 				try
 				{
-					return get_directory_list(current_path);
+					return get_directory_list(temp_path);
 				}
-				catch (std::filesystem::filesystem_error& e){
+				catch (std::filesystem::filesystem_error &e)
+				{
 					log << "Exception occured!!: " << e.what() << "\n";
 					return std::vector<std::string>();
 				}
 			}
-			auto get_selection()
+			else
 			{
-				return selection;
+				//ISSA FILE?
 			}
+		}
+		catch (std::filesystem::filesystem_error &e)
+		{
+			log << "Exception occured!!: " << e.what() << "\n";
+			auto selected = std::filesystem::directory_entry();
+		}
+		return std::vector<std::string>();
+	}
 
-			auto get_left()
-			{
-				auto temp = current_path;
-				if (temp.has_relative_path())
-				{
-					temp = temp.parent_path();
-					auto di = std::filesystem::directory_iterator(temp);
-					std::vector<std::string> res;
-					for (auto &d : di)
-					{
-						res.push_back(d.path().filename().string());
-					}
-					return res;
-				}
-				return std::vector<std::string>();
-			}
+private:
+	int selection = 0;
+	std::filesystem::path current_path;
 
-			auto get_right()
-			{
-				try{
-					auto selected = get_directory_entry(selection);
-					log << "GET RIGHT: " << selected.path().filename().string() << "\n";
+	std::filesystem::directory_entry get_directory_entry(int sel) const
+	{
+		auto di = std::filesystem::directory_iterator(current_path);
+		for (int i = 0; i < sel; i++)
+		{
+			di++;
+		}
+		return *di;
+	}
 
-					if (selected.is_directory())
-					{
-
-						auto temp_path = current_path;
-						temp_path = temp_path / selected.path().filename();
-						try
-						{
-							return get_directory_list(temp_path);
-						}
-						catch (std::filesystem::filesystem_error& e){
-							log << "Exception occured!!: " << e.what() << "\n";
-							return std::vector<std::string>();
-						}
-					}
-					else{
-						//ISSA FILE?
-					}
-				}
-				catch(std::filesystem::filesystem_error& e){
-					log << "Exception occured!!: " << e.what() << "\n";
-					auto selected = std::filesystem::directory_entry();
-				}
-				return std::vector<std::string>();
-			}
-
-		private:
-			int selection = 0;
-			std::filesystem::path current_path;
-
-			std::filesystem::directory_entry get_directory_entry(int sel){
-				auto di = std::filesystem::directory_iterator(current_path);
-				for (int i = 0; i < sel; i++)
-				{
-
-					di++;
-				}
-				return *di;
-			}
-			std::vector<std::string> get_directory_list(std::filesystem::path temp){
-				auto di = std::filesystem::directory_iterator(temp);
-				std::vector<std::string> res;
-				for (auto &d : di)
-				{
-					res.push_back(d.path().filename().string());
-				}
-				return res;
-			}
-	};
+	std::vector<std::string> get_directory_list(std::filesystem::path temp) const
+	{
+		auto di = std::filesystem::directory_iterator(temp);
+		std::vector<std::string> res;
+		for (auto &d : di)
+		{
+			res.push_back(d.path().filename().string());
+		}
+		return res;
+	}
+};
 
 } // namespace ssf
